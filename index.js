e23088923b773fef98b2e49591da63db5fc3abf3
cbyungmal <jgaines554@noscihigh.org>
2021-01-15 18:12:13 +0000

    What I was told.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@
 Declare a variable named hobbies and assign it to an array.
 Then, fill the array with string values of your hobbies
 */
+var hobbies;
 
+hobbies = "'eating', 'sleeping', 'gaming', 'basketball', learning'";
 
 
 
@@ -23,3 +25,5 @@ Fill the array with values of all data types we have learned so far, including a
 
 Data Types include: String, Number, Boolean, and Array.
 */
+var dataTypes;
+dataTypes = ['String','Number', 'Boolean', 'Array', 'Characters', 'Push', 'Unshift', 'Pop'];
